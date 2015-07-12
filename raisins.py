ffa91f08986e5dfbe77af43cbd65595b6f47a71e
Yariv Levi <Yariv.Levi@outlook.com>
2015-07-12 15:30:15 +0300

    adding vcard template

--- raisins.py
+++ raisins.py
@@ -3,6 +3,20 @@ Generate VCARD per employee in Excel file
 """
 import csv
 
+vcard_template = '''
+BEGIN:VCARD
+VERSION:2.1
+N:Bunny;Bugs
+FN:Bugs Bunny
+ORG:Looney Tunes
+TITLE:SVP Carrot Eating
+TEL;WORK;VOICE:555-555-5555
+ADR;WORK:;;1 Carrot Ave.;Los Angels;CA;90583;United States of America
+EMAIL;PREF;INTERNET:bugs@looney.com
+REV:20140609T195243Z
+END:VCARD
+'''
+
 #Get
 fp=open('raisin_team.csv')
 
